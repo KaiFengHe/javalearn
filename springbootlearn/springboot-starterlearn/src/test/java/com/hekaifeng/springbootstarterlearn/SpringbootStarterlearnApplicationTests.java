da1e2eada48c041f0dcf2fc4fd912185666bc3da
417984079@qq.com <417984079@qq.com>
2021-12-18 11:02:34 +0800

    springboot helloworld

---
+++ springbootlearn/springboot-starterlearn/src/test/java/com/hekaifeng/springbootstarterlearn/SpringbootStarterlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hekaifeng.springbootstarterlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStarterlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
